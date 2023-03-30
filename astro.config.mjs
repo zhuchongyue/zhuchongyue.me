e083eab08ee7407b7711cd38a7bc4a30e7c59c51
zhuchongyue <chongyuezhu@gmail.com>
2023-03-30 19:14:55 +0800

    feat: site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://zhuchongyue.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()]
 });
